c6558c86926b117c26c35958724cc902ecbb4a14
engler <ddd.rrr.eee@gmail.com>
2019-01-16 19:13:51 -0800

    fixed

---
+++ labs/lab3-cross-check/simple-cross-check/patch.c
@@ -0,0 +1,17 @@
+// delete these two functions in simple-check.c and put these there instead.
+
+
+static int pin_gen(unsigned *out) {
+        static unsigned u = 0;
+        if(u > 70) {
+                u = 0;
+                return 0;
+        }
+        *out = (u < 64) ? u : random();
+        u++;
+        return 1;
+}
+
+gen_t xc_get_pin_gen(void) {
+        return pin_gen;
+}
