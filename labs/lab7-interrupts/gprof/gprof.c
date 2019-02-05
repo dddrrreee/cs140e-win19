46ddabbd55a969b04265cab23c691134cbfe84fc
engler <ddd.rrr.eee@gmail.com>
2019-02-04 16:04:17 -0800

    simple gprof harness

---
+++ labs/lab7-interrupts/gprof/gprof.c
@@ -0,0 +1,124 @@
+/*
+ * use interrupts to implement a simple statistical profiler.
+ *	- interrupt code is a replication of ../timer-int/timer.c
+ *	- you'll need to implement kmalloc so you can allocate 
+ *	  a histogram table from the heap.
+ *	- implement functions so that given a pc value, you can increment
+ *	  its associated count
+ */
+#include "rpi.h"
+#include "timer-interrupt.h"
+
+
+/**********************************************************************
+ * trivial kmalloc:  
+ *	- do first.  check that its output makes sense!
+ */
+
+// useful for rounding up.   e.g., roundup(n,8) to roundup <n> to 8 byte
+// alignment.
+#define roundup(x,n) (((x)+((n)-1))&(~((n)-1)))
+
+/*
+ * return a memory block of at least size <nbytes>
+ *	- use to allocate gprof code histogram table.
+ *	- note: there is no free, so is trivial.
+ */
+void *kmalloc(unsigned nbytes) {
+	unimplemented();
+}
+
+// one-time called before kmalloc to setup heap.
+void kmalloc_init(void) {
+	unimplemented();
+}
+
+/***************************************************************************
+ * gprof implementation:
+ *	- allocate a table with one entry for each instruction.
+ *	- gprof_init(void) - call before starting.
+ *	- gprof_inc(pc) will increment pc's associated entry.
+ *	- gprof_dump will print out all samples.
+ */
+
+static unsigned gprof_init(void) {
+	unimplemented();
+}
+
+static void gprof_inc(unsigned pc) {
+	unimplemented();
+}
+
+// print out all samples whose count > min_val
+//
+// make sure sampling does not pick this code up!
+static void gprof_dump(unsigned min_val) {
+	unimplemented();
+}
+
+
+
+/***********************************************************************
+ * timer interrupt code from before, now calls gprof update.
+ */
+// Q: if you make not volatile?
+static volatile unsigned cnt;
+static volatile unsigned period;
+
+// client has to define this.
+void int_handler(unsigned pc) {
+	unsigned pending = RPI_GetIRQController()->IRQ_basic_pending;
+
+	// if this isn't true, could be a GPU interrupt: just return.
+	if((pending & RPI_BASIC_ARM_TIMER_IRQ) == 0)
+		return;
+
+        /* 
+	 * Clear the ARM Timer interrupt - it's the only interrupt we have
+         * enabled, so we want don't have to work out which interrupt source
+         * caused us to interrupt 
+	 *
+	 * Q: if we delete?
+	 */
+        RPI_GetArmTimer()->IRQClear = 1;
+	cnt++;
+
+        gprof_inc(pc);
+
+	static unsigned last_clk = 0;
+	unsigned clk = timer_get_time();
+	period = last_clk ? clk - last_clk : 0;
+	last_clk = clk;
+	
+	// Q: if we put a print statement?
+}
+
+// trivial program to test gprof implementation.
+// 	- look at output: do you see weird patterns?
+void notmain() {
+	uart_init();
+	
+	printk("about to install handlers\n");
+        install_int_handlers();
+
+	printk("setting up timer interrupts\n");
+	// Q: if you change 0x100?
+	timer_interrupt_init(0x10);
+        gprof_init();
+
+	// Q: if you don't do?
+	printk("gonna enable ints globally!\n");
+  	system_enable_interrupts();
+	printk("enabled!\n");
+
+	// enable_cache(); 	// Q: what happens if you enable cache?
+        unsigned iter = 0;
+        while(cnt<200) {
+                printk("iter=%d: cnt = %d, period = %dusec, %x\n",
+                                iter,cnt, period,period);
+                iter++;
+                if(iter % 10 == 0)
+                        gprof_dump(2);
+        }
+	clean_reboot();
+}
