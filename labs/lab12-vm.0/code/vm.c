74aeaed6bc2c6c7b13e0595304b05632e7f959f8
engler <ddd.rrr.eee@gmail.com>
2019-02-25 18:41:54 -0800

    code.

---
+++ labs/lab12-vm.0/code/vm.c
@@ -0,0 +1,516 @@
+/*
+	simple code for VM: starter code at the top (mostly).  
+    - your code at the end.  
+    - write your own implementation of any our_* function.
+
+    b4-6: before mmu enabled, the icache should be disabled and invalidated.
+     icache can then be enabled at the same time as the mmu is enabled.
+     clean_inv_caches();
+
+    b4-7: strongly recommended that the code which enables or disables the 
+     mmu has identical virtual and physical addresses.
+*/
+#include "rpi.h"
+#include "vm.h"
+
+/************************************************************************
+ ************************************************************************
+ * Starter code.
+ */
+
+// check bitfield positions.
+#define check_bitfield(T, field, off, nbits) do {                       \
+        union _u {                                                      \
+                T s;                                                    \
+                unsigned u;                                             \
+        } x = (union _u) { .s = (T){ .field = 1 } };                    \
+        unsigned exp = 1<<off;                                          \
+        if(x.u != exp)                                                  \
+                panic("offset wrong: expect %x got %x %s [off=%d,nbits=%d]\n", \
+                                          exp,x.u,#field,off,nbits);        \
+        assert(x.u == exp);                                             \
+                                                                        \
+        unsigned shift = 32 - nbits;                                \
+        unsigned set_all = (~0UL << shift) >> shift;                      \
+        exp = set_all << off;                                           \
+        x.s.field = ~0;                                                 \
+        if(x.u != exp)                                                  \
+                panic("width wrong: expect %x got %x %s [off=%d,nbits=%d]\n", \
+                                          exp,x.u,#field,off,nbits);        \
+        assert(x.u == exp);                                             \
+} while(0)
+
+#define print_field(x, field) do {                   \
+        printk("\t0b%b\t= %s\n", (x)->field, #field);   \
+        if(((x)->field) > 8)                            \
+            printk("\t0x%x\n", (x)->field);             \
+} while(0)
+
+#define is_aligned(x, a)        (((x) & ((typeof(x))(a) - 1)) == 0)
+
+
+/*************************************************************************
+ * control reg 1: b4-40
+ */
+
+// actually: not sure.
+struct control_reg1 control_reg1_mk(void) {
+    struct control_reg1 r;
+    memset(&r, 0, sizeof r);
+    return r;
+}
+
+void check_control_reg1(void) {
+    AssertNow(sizeof(struct control_reg1) == 4);
+    check_bitfield(struct control_reg1, MMU_enabled,        0,      1);
+    check_bitfield(struct control_reg1, A_alignment,        1,      1);
+    check_bitfield(struct control_reg1, C_unified_enable,   2,      1);
+    check_bitfield(struct control_reg1, W_write_buf,        3,      1);
+    check_bitfield(struct control_reg1, B_endian,           7,      1);
+    check_bitfield(struct control_reg1, S_prot,             8,      1);
+    check_bitfield(struct control_reg1, R_rom_prot,         9,      1);
+    check_bitfield(struct control_reg1, F,                  10,      1);
+    check_bitfield(struct control_reg1, Z_branch_pred,      11,      1);
+    check_bitfield(struct control_reg1, I_icache_enable,    12,      1);
+    check_bitfield(struct control_reg1, V_high_except_v,    13,      1);
+    check_bitfield(struct control_reg1, RR_cache_rep,       14,      1);
+    check_bitfield(struct control_reg1, L4,                 15,      1);
+    check_bitfield(struct control_reg1, F1,                 21,      1);
+    check_bitfield(struct control_reg1, U_unaligned,        22,      1);
+    check_bitfield(struct control_reg1, XP_pt,              23,      1);
+    check_bitfield(struct control_reg1, VE_vect_int,        24,      1);
+    check_bitfield(struct control_reg1, EE,                 25,      1);
+    check_bitfield(struct control_reg1, L2_enabled,         26,      1);
+    check_bitfield(struct control_reg1, TR_tex_remap,       28,      1);
+    check_bitfield(struct control_reg1, FA_force_ap,        29,      1);
+}
+
+void control_reg1_valid(struct control_reg1 *r) {
+    // SBO: should be 1
+    assert(r->_unused1 == 0b111);
+    assert(r->_dt == 1);
+    assert(r->_it == 1);
+
+    // SBZ: should be 0
+    assert(r->_sbz0 == 0);
+    assert(r->_sbz1 == 0);
+    assert(r->_st == 0);
+
+    // unknown
+    // assert(r->_reserved == 0);
+
+    // allow non-back compat.  
+    // assert(r->XP_pt == 1);
+
+    // we don't enable cache yet.
+    assert(!r->L2_enabled);
+    assert(!r->I_icache_enable);
+    assert(!r->C_unified_enable);
+}
+    
+void control_reg1_print(struct control_reg1 *r) {
+    control_reg1_valid(r);
+
+    printk("system control reg=\n");
+    print_field(r, FA_force_ap);
+    print_field(r, TR_tex_remap);
+    print_field(r, L2_enabled);
+    print_field(r, EE);
+    print_field(r, VE_vect_int);
+    print_field(r, XP_pt);
+    print_field(r, U_unaligned);
+    print_field(r, F1);
+    print_field(r, RR_cache_rep);
+    print_field(r, I_icache_enable);
+    print_field(r, Z_branch_pred);
+    print_field(r, R_rom_prot);
+    print_field(r, S_prot);
+    print_field(r, B_endian);
+    print_field(r, W_write_buf);
+    print_field(r, C_unified_enable);
+    print_field(r, A_alignment);
+    print_field(r, MMU_enabled);
+}
+
+/*************************************************************************
+ * tlb config: b4-39
+ */
+static void check_tlb_config(void) { 
+    AssertNow(sizeof(struct tlb_config) == 4);
+    check_bitfield(struct tlb_config, unified_p,    0,      1);
+    check_bitfield(struct tlb_config, n_d_lock,     8,      8);
+    check_bitfield(struct tlb_config, n_i_lock,    16,      8);
+}
+struct tlb_config get_tlb_config(void) {
+    return read_tlb_config();
+}
+void tlb_config_print(struct tlb_config *c) {
+    printk("TLB config:\n");
+    printk("\tunified=%d\n", c->unified_p);
+    printk("\tlockable data ent=%d\n", c->n_d_lock);
+    printk("\tlockable inst ent=%d\n", c->n_i_lock);
+}
+
+/*************************************************************************
+ *************************************************************************
+ * the rest is your code.
+ */
+
+/*************************************************************************
+ * part0
+ */
+
+struct cache_size { 
+    unsigned char len, m, assoc, size, p;
+};
+
+// implemement this.
+static struct cache_size get_size(unsigned u) {
+    unimplemented();
+
+    struct cache_size s;
+
+    // b6-15
+    s.len = u&0b11;
+    s.m = (u >> 2)&0b1;
+    s.assoc = (u >> 3) & 0b111;
+    s.size = (u >> 6) & 0b1111;
+    s.p = (u >> 11)&0b1;
+    return s;
+}
+
+// can replace these switch statements with calculation.
+static void cache_size_print(const char *msg, struct cache_size s) {
+    printk("%s:\n", msg);
+    // printk("\tlen=%d\n", s.len);
+    printk("\tm=%d\n", s.m);
+
+    unsigned K = 0;
+    switch(s.size) {
+    case 0b00: 
+    case 0b01: 
+    case 0b10: 
+    case 0b11: 
+        panic("too small\n");
+
+    case 0b100: K = !s.m ? 8 : 12; break;
+    case 0b101: K = !s.m ? 16 : 24; break;
+    case 0b110: K = !s.m ? 32 : 48; break;
+    case 0b111: K = !s.m ? 64 : 96; break;
+    case 0b1000: K = !s.m ?128 : 192; break;
+    default:
+        panic("invalid size\n");
+    }
+    printk("\tcache size=%dK\n", K);
+
+    unsigned W = 0;
+    switch(s.len) {
+    case 0: W = 2; break;
+    case 1: W = 4; break;
+    case 2: W = 8; break;
+    case 3: W = 16; break;
+    default: panic("invalid len\n");
+    }
+    printk("\tcache line length = %d words\n", W);
+
+    unsigned A = 0;
+    switch(s.assoc)  {
+    case 0: A = !s.m ? 1 : 0; break;
+    case 1: A = !s.m ? 2 : 3; break;
+    case 2: A = !s.m ? 4 : 6; break;
+    case 3: A = !s.m ? 8 : 12; break;
+    case 4: A = !s.m ? 16 : 24; break;
+    default: panic("not handling assoc=%d\n", s.assoc);
+    }
+    printk("\tassoc = %d-way\n", A);
+    if(s.p)
+        printk("\tpage allocation restriction\n");
+    else
+        printk("\tpage allocation not restricted\n");
+}
+
+static void part0(void) {
+
+    printk("***************** part 0 START *********************\n");
+    unsigned u = get_cache_type();
+
+    printk("cache type: %b\n", u);
+    assert(u & (1<<24));
+    printk("I/D cache\n");
+
+                   // 109876543210  12 bits
+    unsigned mask = 0b111111111111;
+    struct cache_size isize = get_size(u & mask);
+    struct cache_size dsize = get_size((u >> 12) & mask);
+    cache_size_print("instruction cache", isize);
+    cache_size_print("data cache", dsize);
+    printk("***************** part 0 END *********************\n");
+}
+
+/************************************************************************
+ * Part1: implement FLD.
+ */
+
+fld_t fld_mk(void) {
+    fld_t f;
+    // all unused fields can have 0 as default.
+    memset(&f, 0, sizeof f);
+    f.tag = 0b10;
+    return f;
+}
+
+void fld_check_valid(fld_t *f) {
+    assert(f->_sbz1 == 0);
+    assert(f->tag == 0b10);
+    assert(f->S == 0);
+    assert(f->IMP == 0);
+    assert(f->C == 0);
+    assert(f->B == 0);
+    assert(f->super == 0);
+}
+
+void fld_check(void) {
+    fld_t f = fld_mk();
+    fld_check_valid(&f);
+
+    AssertNow(sizeof f == 4);
+
+    //                    field     offset  nbits
+    check_bitfield(fld_t, tag,      0,      2);
+    check_bitfield(fld_t, B,        2,      1);
+    check_bitfield(fld_t, C,        3,      1);
+    check_bitfield(fld_t, XN,       4,      1);
+    check_bitfield(fld_t, domain,   5,      4);
+    check_bitfield(fld_t, IMP,      9,      1);
+    check_bitfield(fld_t, AP,       10,     2);
+    check_bitfield(fld_t, TEX,      12,     3);
+    check_bitfield(fld_t, APX,      15,     1);
+    check_bitfield(fld_t, S,        16,     1);
+    check_bitfield(fld_t, nG,       17,     1);
+    check_bitfield(fld_t, super,       18,     1);
+    check_bitfield(fld_t, sec_base_addr, 20,     12);
+}
+
+void fld_print(fld_t *f) {
+    printk("------------------------------\n");
+    printk("0x%x:\n", f);
+    print_field(f, sec_base_addr);
+    printk("\t  --> va=0x%8x\n", f->sec_base_addr<<20);
+    printk("\t           76543210\n");
+
+    print_field(f, nG);
+    print_field(f, S);
+    print_field(f, APX);
+    print_field(f, TEX);
+    print_field(f, AP);
+    print_field(f, IMP);
+    print_field(f, domain);
+    print_field(f, XN);
+    print_field(f, C);
+    print_field(f, B);
+    print_field(f, tag);
+
+    fld_check_valid(f);
+}
+
+void fld_set_base_addr(fld_t *f, unsigned addr) {
+    demand(is_aligned(addr,20), addr is not aligned!);
+
+    // unimplemented();
+    f->sec_base_addr = addr >> 20;
+
+    // if the previous code worked, this should always succeed.
+    assert((f->sec_base_addr << 20) == addr);
+}
+
+
+/*************************************************************************
+ * tlbr  b4-41
+ */
+#if 0
+void ttbr_set(struct tlb_reg *r, unsigned base, unsigned rgn, unsigned n) {
+    assert(n == 0);
+    assert(is_aligned(base,14));
+    r->base = base | (rgn << 3);
+    
+}
+#endif
+
+void ttbr0_get(unsigned *base, unsigned *rgn, unsigned n) {
+    assert(n == 0);
+    // assert(read_tlb_base_ctrl() == 0);
+
+    unsigned r = read_ttbr0().base;
+    *rgn = (r >> 3) & 0b11;
+    *base = (r >> 14) << 14;
+
+}
+
+void ttbr1_get(unsigned *base, unsigned *rgn, unsigned n) {
+    assert(n == 0);
+    //assert(read_tlb_base_ctrl() == 0);
+
+    unsigned r = read_ttbr1().base;
+    *rgn = (r >> 3) & 0b11;
+    *base = (r >> 14) << 14;
+    // assert(*base == 0);
+}
+
+static void tlbr_print(void) {
+    unsigned base0, rgn0, base1, rgn1;
+    ttbr0_get(&base0, &rgn0, 0);
+    ttbr1_get(&base1, &rgn1, 0);
+    printk("TLBR registers:\n");
+    printk("\ttlb base control: N=%b\n", read_tlb_base_ctrl());
+    printk("\tTTBR0: base=%x, rgn=%b\n", base0, rgn0);
+    printk("\tTTBR1: base=%x, rgn=%b\n", base1, rgn1);
+}
+
+
+/*************************************************************************
+ * domain access control  b4-42
+ */
+
+void domain_acl_print(void) {
+    printk("domain access control:\n");
+    printk("\t%b\n", read_domain_access_ctrl());
+}
+
+#if 0
+void domain_acl_set(unsigned domain_id, unsigned perm) {
+    // we aren't doing anything fancy.
+    assert(domain_id == 0);
+
+    // always
+    assert((perm & 0b11) == perm);
+    assert(domain_id < 16);
+
+    // unneeded, but this is what we'd do w/ more domains.
+    unsigned u = read_domain_access_ctrl();
+    u &= ~(0b11<<domain_id);
+    u |= perm << domain_id;
+
+    // since only doing 0
+    assert(u == perm);
+    our_write_domain_access_ctrl(u);
+}
+#endif
+
+/**********************************************************************
+ * ASID code
+ */
+struct tlb_procid {
+    unsigned asid:8,
+             pid:24;
+};
+
+static void tlb_get_pid(unsigned *pid, unsigned *asid) {
+    unsigned procid = tlb_get_procid();
+    *pid = procid >> 8;
+    *asid = procid & 0xff;
+}
+
+static void tlb_asid_print(void) {
+    unsigned pid, asid;
+    tlb_get_pid(&pid,&asid);
+    printk("tlb procid = %x,  asid = %d\n", pid, asid);
+}
+
+// create a mapping for <va> to <pa> in the page table <pt>
+void mmu_map_section(fld_t *pt, unsigned va, unsigned pa) {
+    assert(is_aligned(va, 20));
+    assert(is_aligned(pa, 20));
+
+    // set to the right index in pt.
+    fld_t *pte = 0;
+
+    unimplemented();
+
+    fld_print(pte);
+    printk("my.pte@ 0x%x = %b\n", pt, *(unsigned*)pte);
+}
+
+// would want to initialize everything.
+fld_t *mmu_init(unsigned base) {
+    demand(is_aligned(base, 14), must be 14 bit aligned!);
+
+    fld_t *pt = (void*)base;
+    memset(pt, 0, 4096 * sizeof *pt);
+
+    // should do one-time invalidation of all caches, etc.
+    return pt;
+}
+
+
+void part1(void) {
+    printk("******** part 1! ************\n");
+    unsigned base = 0x100000;
+
+    fld_t *pt = mmu_init(base);
+
+    // only mapping a single section: do more.
+    mmu_map_section(pt, 0x0, 0x0);
+    mmu_map_section(pt, 0x20000000, 0x20000000);
+    mmu_map_section(pt, 0x20200000, 0x20200000);
+
+    // this should be wrapped up neater.  broken down so can replace 
+    // one by one.
+
+    struct control_reg1 c1 = read_control_reg1();
+    c1.XP_pt = 1;
+    write_control_reg1(c1);
+    c1 = read_control_reg1();
+    assert(c1.XP_pt);
+    assert(!c1.MMU_enabled);
+
+    our_write_domain_access_ctrl(~0UL);
+    dsb();
+
+    // use the sequence on B2-25
+    our_set_procid_ttbr0(1, pt);
+
+    // have to flush I/D cache and TLB, BTB, prefetch buffer.
+    c1.MMU_enabled = 1;
+    our_mmu_enable(c1);
+    
+    // mmu_enable(c1, pt);
+
+    // VM ON!
+    c1 = read_control_reg1();
+    assert(c1.MMU_enabled);
+
+    int x = 5, v0, v1;
+    v0 = get32(&x);
+    printk("doing print with vm ON\n");
+    x++;
+    v1 = get32(&x);
+
+    // turn off.
+    c1.MMU_enabled = 0;
+    our_mmu_disable(c1);
+
+    c1 = read_control_reg1();
+    assert(!c1.MMU_enabled);
+    printk("OFF\n");
+
+    // our reads worked.
+    assert(v0 == 5);
+    assert(v1 == 6);
+    printk("******** success ************\n");
+}
+
+/**********************************************************************
+ * implement part0, part1, part2.
+ */
+
+void notmain() {
+    uart_init();
+    printk("implement one at a time.\n");
+    part1();
+    clean_reboot();
+
+    // move about reboot and implement
+    part0();
+
+	clean_reboot();
+}
---
+++ labs/lab12-vm.0/code/vm.h
@@ -0,0 +1,204 @@
+#ifndef __VM_H__
+#define __VM_H__
+
+void inv_icache(void);
+void inv_dcache(void);
+
+void inv_caches(void);
+void clean_inv_caches(void);
+
+/*
+    -----------------------------------------------------------------
+    b4-26 first level descriptor  
+        bits0:1 =
+            - 0b00: unmapped, 
+            - 0b10: section descriptor, 
+            - 0b01: second level coarse pt.
+            - 0b11: reserved
+
+    b4-27 section:
+        31-20: section base address: must be aligned.
+        19: sbz: "should be zero"
+        18: 0
+        17: nG:  b4-25, global bit=0 implies global mapping, g=1, process 
+                 specific.
+        -16: S: 0 deprecated.
+        -15: APX  0b11 r/w [see below]
+        -14-12: TEX
+        -11-10: AP 
+        9: IMP: 0: should be set to 0 unless the implementation defined
+                   functionality is required.
+
+        -8-5: Domain: b4-10: 0b11 = manager (no perm check), 0b01 (checked perm)
+        -4: XN: 1 = execute never,  0 = can execute.
+        3:C: set 0 see below
+        2:B: set 0
+        1: 1
+        0: 0
+
+  APX, S, R: 
+    b4-8: permissions, table on b4-9
+    use of S/R deprecated.
+
+    S  R  APX   AP[1:0]   privileged permissions   user permissions
+    x  x    0   0b11       r/w                       r/w
+    x  x    0   0b10       r/w                       r/o
+    x  x    0   0b01       r/w                       no access
+    0  0    0      0       no access                no access
+    
+  ap,apx,domain:
+     b4-8
+
+  c,b,tex:
+   b4-11, b4-12 
+
+    TEX   C  B 
+    0b000 0  0 strongly ordered.   
+    0b001 0  0 non-cacheable
+
+
+*/
+
+// on pi: organized from 0 to high.
+typedef struct first_level_descriptor {
+    unsigned
+        tag:2,      // 0-1:2    should be 0b10
+        B:1,        // 2:1      set to 0
+        C:1,        // 3:1      set to 0
+        XN:1,       // 4:1      1 = execute never, 0 = can execute
+        domain:4,   // 5-8:4    b4-10: 0b11 = manager, 0b01 checked perms
+        IMP:1,      // 9:1      should be set to 0 unless imp-defined 
+                    //          functionality is needed.
+
+        AP:2,       // 10-11:2  permissions, see b4-8/9
+        TEX:3,      // 12-14:3
+        APX:1,      // 15:1     
+        S:1,        // 16:1     set=0, deprecated.
+        nG:1,       // 17:1     nG=0 ==> global mapping, =1 ==> process specific
+        super:1,    // 18:1     selects between section and supersection (0)
+        _sbz1:1,    // 19:1     sbz
+        sec_base_addr:12; // 20-31.  must be aligned.
+} fld_t;
+
+
+// 3-60
+
+// b4-39
+struct tlb_config {
+    unsigned
+        unified_p:1,    // 0:1 0 = unified, 1 = seperate I/D
+        _sbz0:7,        // 1-7:7
+        n_d_lock:8,       // 8-15:8  number of unified/data lockable entries
+        n_i_lock:8,        //16-23:8 number of instruction lockable entries
+        _sbz1:8;
+};
+struct tlb_config read_tlb_config(void);
+
+// b3-12
+// SBO = should be 1.
+// A more full description on 3-45 in arm1176.pdf  
+typedef struct control_reg1 {
+    unsigned
+        MMU_enabled:1,      // 0:1,   0 = MMU disabled, 1 = enabled,
+        A_alignment:1,      // 1:1    0 = alignment check disabled.
+        C_unified_enable:1, // 2:1    if unified used, this is enable/disable
+                            //        otherwise is enable/disable for dcache
+        W_write_buf:1,      // 3:1    0 = write buffer disabled
+        _unused1:3,         // 4:3
+        B_endian:1,         // 7:1    0 = little 
+        S_prot:1,           // 8:1   - deprecated b4-8
+        R_rom_prot:1,       // 9:1   - deprecated b4-8
+        F:1,                // 10:1  impl defined
+        Z_branch_pred:1,    // 11:1  branch predict 0 = disabled, 1 = enabled
+        I_icache_enable:1,  // 12:1  if seperate i/d, disables(0) or enables(1)
+        V_high_except_v:1,  // 13:1  location of exception vec.  0 = normal
+        RR_cache_rep:1,     // 14:1 cache replacement 0 = normal, 1 = different.
+        L4:1,               // 15:1 inhibits arm internworking.
+        _dt:1,              // 16:1, SBO
+        _sbz0:1,            // 17:1 sbz
+        _it:1,              // 18
+        _sbz1:1,            // 19
+        _st:1,              // 20
+        F1:1,               // 21   fast interrupt.  [dunno]
+        U_unaligned:1,      // 22 : unaligned
+        XP_pt:1,            // 23:1,  1 = vmsav6, subpages disabled.
+        VE_vect_int:1,      // 24: no.  0
+        EE:1,               // 25 endient exception a2-34 
+        L2_enabled:1,       // 26,
+        _reserved0:1,       // 27
+        TR_tex_remap:1,     // 28
+        FA_force_ap:1,      // 29  0 = disabled, 1 = force ap enabled      
+        _reserved1:2;
+} ctrl_reg1_t;
+
+ctrl_reg1_t our_read_control_reg1(void);
+void our_write_control_reg1(ctrl_reg1_t r);
+ctrl_reg1_t read_control_reg1(void);
+void write_control_reg1(ctrl_reg1_t r);
+
+// C, S, IMP, should be 0 and the offset can dynamically change based on N,
+// so we just do this manually.
+struct tlb_reg {
+#if 0
+    unsigned 
+        c:1,    // 0:1  inner cacheabled = 1, non=0
+        s:1,    // 1:1, pt walk is shareable 1 or non-sharable (0)
+        IMP:1,  // 2:1,
+        RGN:2,  // 3:2 // B4-42: is it cacheable: 
+                       // 0b00: outer non-cacheaable.  
+                       // 0b10 outer write through
+                       // 0b11 outer write back
+        // this can change depending on what N is.  not sure the cleanest.
+        base:
+#endif
+    unsigned base;
+};
+
+struct tlb_reg read_ttbr0(void);
+void write_ttbr0(struct tlb_reg r);
+
+struct tlb_reg read_ttbr1(void);
+void write_ttbr1(struct tlb_reg r);
+
+// read the N that divides the address range.  0 = just use ttbr0
+unsigned read_tlb_base_ctrl(void);
+
+// set the N that divides the address range b4-41
+void write_tlb_base_ctrl(unsigned N);
+
+
+
+void tlb_invalidate(void);
+void tlb_i_invalidate(void);
+void tlb_d_invalidate(void);
+
+int switch_addr(struct control_reg1 on, struct control_reg1 off, void*);
+int enable_mmu(struct control_reg1 on, struct control_reg1 off, void*);
+
+// lower 8 bits are ASID
+void our_tlb_set_procid(unsigned proc_id);
+void tlb_set_procid(unsigned proc_id);
+
+unsigned tlb_get_procid(void);
+
+unsigned read_domain_reg(void);
+void write_domain_reg(unsigned);
+
+unsigned get_cache_type(void);
+unsigned our_get_cache_type(void);
+
+void our_set_procid_ttbr0(unsigned procid, fld_t *pt);
+void set_procid_ttbr0(unsigned procid, fld_t *pt);
+
+void our_mmu_disable(struct control_reg1 off);
+void our_mmu_enable(ctrl_reg1_t c);
+void mmu_enable(ctrl_reg1_t c);
+void mmu_disable(struct control_reg1 off);
+
+// 16 2 bit access control fields.
+unsigned read_domain_access_ctrl(void);
+
+void our_write_domain_access_ctrl(unsigned d);
+
+#endif
+
